aa2b0fae8252b87397a32f939a2e36e3dbe903ef
Bhargav-Wappnet <bhargav.wappnet@gmail.com>
2023-03-21 19:39:47 +0530

    Done List_Published_Blog API

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from blog.views import CreateBlogAPIView
+from blog.views import CreateBlogAPIView, BlogListAPIView
 
 urlpatterns = [
     # Creat-blog API endpoint
     path("create-blog/",
          CreateBlogAPIView.as_view(),
          name="createblog"),
+    
+    # List-blogs Published API endpoint
+    path("list-all-blogs/",
+         BlogListAPIView.as_view(),
+         name="listallblogs"),
 ]
